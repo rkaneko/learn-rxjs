5265ee90a99a6dc00e4c4932407fa0956e5c6725
rkaneko <kanek.o.dev@gmail.com>
2016-12-26 21:42:01 +0900

    `add` example for understanding Subscription

---
+++ src/Subscription/unsubscribe-multi-subscription.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Observable = require('rxjs/Observable').Observable;
+
+const observable1 = Observable.create(function subscribe(observer) {
+  const intervalID = setInterval(() => {
+    observer.next('hey!');
+  }, 500);
+
+  return function unsubscribe() {
+    console.log('unsubscribing 1');
+    clearInterval(intervalID);
+  };
+});
+
+const observable2 = Observable.create(function subscribe(observer) {
+  const intervalID = setInterval(() => {
+    observer.next('hey child!');
+  }, 500);
+
+  return function unsubscribe() {
+    console.log('unsubscribing 2');
+    clearInterval(intervalID);
+  };
+});
+
+const subscription = observable1.subscribe(x => console.log(x));
+const childSubscription = observable2.subscribe(x => console.log(x));
+
+subscription.add(childSubscription);
+
+const intervalID = setTimeout(() => {
+  subscription.unsubscribe();
+  clearInterval(intervalID);
+}, 3000);
